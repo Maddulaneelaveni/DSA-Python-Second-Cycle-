9f0d6468a7cdbffa70fe98d65280aee188bfe740
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-06-02 22:00:00 +0530

    Insertion sort is added

--- InsertionSort/Basics.py
+++ InsertionSort/Basics.py
@@ -7,3 +7,6 @@
 # If the current element is smaller than the previous elements, shift the larger elements one position to the right.
 # Insert the current element into its correct position in the sorted portion of the array.
 # Repeat the process for all elements until the entire array is sorted.
+# Key Idea: The current element is compared with the sorted portion of the array and inserted into its correct position.
+# Example : [5, 3, 8, 4, 2]
+# Pass 1: 5 3 8 4 2 → 3 5 8 4 2
