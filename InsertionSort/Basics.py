f4c358586b2a86e6a4292f00dd29e8f405d640cf
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-06-17 22:00:00 +0530

    Insertion sort is added

--- InsertionSort/Basics.py
+++ InsertionSort/Basics.py
@@ -1,2 +1,4 @@
 # Insertion sort part-1 -- HAckerank
 # What is Insertion Sort?
+# Insertion Sort builds the sorted array one element at a time by repeatedly taking the next unsorted element and inserting it into the correct position in the already sorted portion of the array.
+# Time Complexity: O(n²)
