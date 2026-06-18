a9d1e80cde05fcdbc0b87ea9b5390ea41ceb7655
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-06-18 22:00:00 +0530

    Insertion sort is added

--- InsertionSort/Basics.py
+++ InsertionSort/Basics.py
@@ -13,3 +13,15 @@
 # Pass 2: 3 5 8 4 2 → 3 5 8 4 2 (no change)
 # Pass 3: 3 5 8 4 2 → 3 4 5 8 2
 # Pass 4: 3 4 5 8 2 → 2 3 4 5 8
+
+# Code :
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
