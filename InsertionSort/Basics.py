b3318a3e0de3afda9ee9e969e110ddddc6dd2a20
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-06-22 22:00:00 +0530

    Insertion sort is added

--- InsertionSort/Basics.py
+++ InsertionSort/Basics.py
@@ -34,12 +34,4 @@ def insertion_sort(arr):
 # Problem Statement:
 # Given an array of integers, sort the array using the insertion sort algorithm and print the array after each iteration of the outer loop.
 # Takes only the last element and inserts it in the correct position.
-def insertionSort1(n, arr):
-    value = arr[-1]
-    i = n - 2
-    while i >= 0 and arr[i] > value:
-        arr[i + 1] = arr[i]
-        print(*arr)
-        i -= 1
-    arr[i + 1] = value
-    print(*arr)
+
