d0c376e07e133f827e3e145eb12cc6cf49f29133
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-06-23 22:00:00 +0530

    Insertion sort is added

---
+++ MergeSort/Leetcode.py
@@ -0,0 +1,15 @@
+# 1. 912- Leetcode Problem- Sort an Array
+
+You are given an unsorted array.
+
+nums = [5, 2, 3, 1]
+
+You need to sort it in ascending order.
+
+Expected output:
+
+[1, 2, 3, 5]
+Constraints
+You cannot simply use sort().
+Interviewers expect you to implement a sorting algorithm.
+Merge Sort is one of the best solutions because it runs in O(n log n).
