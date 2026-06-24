d6c4286422537bd48d9bb16b28670892cb924963
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-06-24 22:00:00 +0530

    Merge sort is added

--- MergeSort/Leetcode.py
+++ MergeSort/Leetcode.py
@@ -9,3 +9,32 @@
 # You cannot simply use sort().
 # Interviewers expect you to implement a sorting algorithm.
 # Merge Sort is one of the best solutions because it runs in O(n log n).
+
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+    mid = len(nums) // 2
+    left = merge_sort(nums[:mid])
+    right = merge_sort(nums[mid:])
+    return merge(left, right)
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+nums = [5, 2, 3, 1]
+sorted_nums = merge_sort(nums)
+print("Sorted Array:", sorted_nums)
