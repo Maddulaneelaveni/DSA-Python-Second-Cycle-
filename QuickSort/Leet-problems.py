7aa1508dcea79bdc5c6fecd550ca932c1bbaa3dd
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-06-26 22:00:00 +0530

    Quick Sort is added

--- QuickSort/Leet-problems.py
+++ QuickSort/Leet-problems.py
@@ -14,3 +14,14 @@
 # Best Case	    O(n log n)	       Pivot divides the array into nearly equal halves at each step.
 # Average Case	O(n log n)	       Random pivots usually produce reasonably balanced partitions.
 # Worst Case	O(n²)	            Pivot is always the smallest or largest element, creating highly unbalanced partitions (e.g., one side has n-1 elements and the other has 0).
+
+# Why is the average case O(n log n)?
+# At each recursive level:
+# Partitioning all elements takes O(n) time.
+# The array is split into roughly half-sized parts.
+# The number of levels is about log n.
+So:
+
+O(n) × O(log n)
+
+= O(n log n)
