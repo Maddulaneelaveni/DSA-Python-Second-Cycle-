9ae6737581db660af45dcb1c57b2cb97f49ffd5c
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-06-29 20:00:00 +0530

    Quick Sort is added

--- QuickSort/Leet-problems.py
+++ QuickSort/Leet-problems.py
@@ -117,5 +117,20 @@ class Solution:
         return [x for x,_ in count.most_common(k)]
     
 
-
+# Sort Colours -Leetcode
+class Solution:
+    def sortColors(self, nums):
+        low = 0
+        mid = 0
+        high = len(nums)-1
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
 
