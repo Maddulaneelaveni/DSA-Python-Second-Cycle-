46652987a89cdba0232a6a7cfe66402403edeefe
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-07-11 22:00:00 +0530

    Normal Counting sort code is  added

--- CountingSort/Normal_CountingSort-code.py
+++ CountingSort/Normal_CountingSort-code.py
@@ -1,18 +1,20 @@
 def counting_sort(arr):
+
+    # Find the maximum element
     max_val = max(arr)
+    # Create count array
     count = [0] * (max_val + 1)
-    # Count frequency
+    # Count frequency of each element
     for num in arr:
         count[num] += 1
+    # Index to place sorted elements
     index = 0
-    # Place elements back into array
+    # Traverse count array
     for i in range(len(count)):
+        # Place each element based on its frequency
         while count[i] > 0:
             arr[index] = i
             index += 1
             count[i] -= 1
+    # Return sorted array
     return arr
-arr = [4, 2, 2, 8, 3, 3, 1]
-print(counting_sort(arr))
-
-# Output : [1, 2, 2, 3, 3, 4, 8]
