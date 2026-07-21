510ca6c64f7cf8ae25d9334a7d7ec3370684e603
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-07-21 22:00:00 +0530

    Remove Duplicates problem is  added

---
+++ Two_Pointers/Remove_Duplicates-problem.py
@@ -0,0 +1,16 @@
+# 3. Same Direction
+# Remove Duplicates
+
+def remove_duplicates(nums):
+    if not nums:
+        return 0
+    slow = 0
+    for fast in range(1, len(nums)):
+        if nums[fast] != nums[slow]:
+            slow += 1
+            nums[slow] = nums[fast]
+
+    return slow + 1
+nums = [1, 1, 2, 2, 3]
+k = remove_duplicates(nums)
+print(nums[:k])
