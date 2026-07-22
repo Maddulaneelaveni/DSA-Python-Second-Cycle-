2dc9ef66478925210f7e16afc06cdceb47a3f62f
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-07-22 22:00:00 +0530

    Move Zeroes problem is  added

---
+++ Two_Pointers/Move_Zeroes-problem.py
@@ -0,0 +1,10 @@
+# 4. Same Direction
+# Move Zeroes
+def move_zeroes(nums):
+    slow = 0
+    for fast in range(len(nums)):
+        if nums[fast] != 0:
+            nums[slow], nums[fast] = nums[fast], nums[slow]
+            slow += 1
+    return nums
+print(move_zeroes([0, 1, 0, 3, 12]))
