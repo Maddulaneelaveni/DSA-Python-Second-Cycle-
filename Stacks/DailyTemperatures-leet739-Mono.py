532a73f461fc7e068a116d4dcfbcacf4c744d038
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-07-27 22:00:00 +0530

    Daily temperature problem using monotonic decreasing stack is  added

---
+++ Stacks/DailyTemperatures-leet739-Mono.py
@@ -0,0 +1,11 @@
+# LeetCode 739. Daily Temperatures
+
+# Approach: Monotonic Decreasing Stack (Indices)
+# Idea :
+# Store indices in the stack, not temperatures.
+# The stack keeps temperatures in decreasing order.
+# If today's temperature is greater than the temperature at the stack's top index:
+# We've found the next warmer day.
+# The answer is:
+# current_index - previous_index
+# Push the current day's index onto the stack.
