a155f843b3fbb79b5f72dde957b7b6dc95e8633e
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-07-30 22:00:00 +0530

    Maximum Sum Of Subarray Of Size K problem is  added

---
+++ SlidingWindow/MaximumSumOfSubarrayOfSizeK.py
@@ -0,0 +1,21 @@
+def maximum_sum_subarray(nums, k):
+    n = len(nums)
+    # Sum of first window
+    window_sum = sum(nums[:k])
+    max_sum = window_sum
+    # Slide the window
+    for i in range(k, n):
+        window_sum += nums[i]
+        window_sum -= nums[i - k]
+
+        if window_sum > max_sum:
+            max_sum = window_sum
+    return max_sum
+# Example
+nums = [2, 1, 5, 1, 3, 2]
+k = 3
+print(maximum_sum_subarray(nums, k))
+
+# Complexity
+# Time: O(n)
+# Space: O(1)
