2f7cb6e93279d52eaa546fa087fd7db51ae8d0c3
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-07-31 22:00:00 +0530

    Maximum No.of vowels in an Subarray Of Size K leetcode problem 1456 is  added

---
+++ SlidingWindow/MaxNo.OfVowelInAnSubarrayOfSizeK-leet1456.py
@@ -0,0 +1,24 @@
+# Problem Statement :
+
+# Given a string s and an integer k, return the maximum number of vowels in any substring of length k.
+
+# Vowels are: a, e, i, o, u.
+
+# Example
+# Input:
+# s = "abciiidef"
+# k = 3
+
+# Output:
+# 3
+
+# Explanation:
+# Substrings of size 3:
+
+# abc → 1 vowel
+# bci → 1 vowel
+# cii → 2 vowels
+# iii → 3 vowels  ← Maximum
+# iid → 2 vowels
+# ide → 2 vowels
+# def → 1 vowel
