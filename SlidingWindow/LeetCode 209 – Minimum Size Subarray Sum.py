3e9660d23364d418505e549981910fef11e1638c
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-08-01 22:00:00 +0530

    Minimum size subarray sum using variable size sliding window leetcode problem 209 is  added

--- SlidingWindow/LeetCode 209 – Minimum Size Subarray Sum.py
+++ SlidingWindow/LeetCode 209 – Minimum Size Subarray Sum.py
@@ -23,3 +23,28 @@
 # Once window_sum >= target,
 # Update the minimum length.
 # Shrink the window from the left to see if we can still satisfy the condition with a smaller window.
+
+
+# Code :
+
+def min_subarray_length(nums, target):
+    left = 0
+    window_sum = 0
+    min_len = float("inf")
+    for right in range(len(nums)):
+        # Expand window
+        window_sum += nums[right]
+        # Shrink window while condition is satisfied
+        while window_sum >= target:
+            # Update answer
+            min_len = min(min_len, right - left + 1)
+            # Remove left element
+            window_sum -= nums[left]
+            left += 1
+    if min_len == float("inf"):
+        return 0
+    return min_len
+# Example
+nums = [2,3,1,2,4,3]
+target = 7
+print(min_subarray_length(nums, target))
